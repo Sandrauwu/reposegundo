8389d005dccdd0d3ccfb992bb71015f300f9ee50
Yisuslalala <1930318@upv.edu.mx>
2019-10-29 00:53:59 -0600

    segundo

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'chana' );
 define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'f163a88eca7ad22c9cbdd6a212' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
